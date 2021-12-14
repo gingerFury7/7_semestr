3b3013a1c70662771612c0aefd040e354f508206
gingerFury7 <slavka2000@mail.ru>
2021-12-14 18:40:05 +0300

    commit

--- 3_QuickSort/dualPivotQuckSort.h
+++ 3_QuickSort/dualPivotQuckSort.h
@@ -49,3 +49,52 @@ void dualPivotQuickSort (int *array, int low, int high){
     dualPivotQuickSort(array, j + 1, g - 1);
     dualPivotQuickSort(array, g + 1, high);
 }
+
+int partition (int *array, int low, int high, int *lp){
+    if (array[low] > array[high])
+        swap (array[low], array[high]);
+
+    int j = low + 1;
+    int g = high - 1;
+    int k = low + 1;
+
+    int leftPivot = array[low];
+    int rightPivot = array[high];
+
+    while (k <= g){
+        if (array[k] < leftPivot){
+            swap(array[k], array[j]);
+            j++;
+        }
+        else if (array[k] >= g) {
+            while (array[g] > rightPivot && k < g)
+                g--;
+            swap(array[k], array[g]);
+            g--;
+            if (array[k] < leftPivot){
+                swap(array[k], array[j]);
+                j++;
+            }
+        }
+        k++;
+    }
+    j--;
+    g++;
+
+    swap(array[low], array[j]);
+    swap(array[high], array[g]);
+
+    *lp = j;
+
+    return g;
+}
+
+void dualPivotQuickSort_2 (int *array, int low, int high){
+    if (low < high){
+        int lp, rp;
+        rp = partition(array, low, high, &lp);
+        dualPivotQuickSort_2(array, low, lp - 1);
+        dualPivotQuickSort_2(array, lp + 1, rp - 1);
+        dualPivotQuickSort_2(array, rp + 1, high);
+    }
+}
--- 3_QuickSort/main.cpp
+++ 3_QuickSort/main.cpp
@@ -5,21 +5,22 @@
 void printArray(int *array, int n)
 {
     for (int i = 0; i < n; ++i)
-        std::cout << array[i] << std::endl;
+        std::cout << array[i] << ' ';
+    std::cout << std::endl;
 }
 
 int main()
 {
     int array[] = {95, 45, 48, 98, 1, 485, 65, 478, 1, 2325};
-    int n = sizeof(array)/sizeof(array[0]);
+    int n = 10;
 
-    std::cout << "Before Quick Sort :" << std::endl;
+    std::cout << "Before Quick Sort:" << std::endl;
     printArray(array, n);
 
-//    quickSort(array, 0, n);
-    dualPivotQuickSort(array, 0, n - 1);
+    quickSort(array, 0, n);
+//    dualPivotQuickSort_2(array, 0, n);
 
-    std::cout << "After Quick Sort :" << std::endl;
+    std::cout << "After Quick Sort:" << std::endl;
     printArray(array, n);
     return (0);
 
